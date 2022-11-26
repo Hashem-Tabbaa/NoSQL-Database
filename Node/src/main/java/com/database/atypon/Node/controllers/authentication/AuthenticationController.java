0d62a1026533e4487f830407aee125fa19cede13
hashem <74487162+Hashem-Tabbaa@users.noreply.github.com>
2022-11-27 01:32:33 +0300

    Updates on the test DBMS

--- Node/src/main/java/com/database/atypon/Node/controllers/authentication/AuthenticationController.java
+++ Node/src/main/java/com/database/atypon/Node/controllers/authentication/AuthenticationController.java
@@ -1,15 +1,12 @@
 package com.database.atypon.Node.controllers.authentication;
 
+import com.database.atypon.Node.model.User;
 import com.database.atypon.Node.services.authentication.AuthenticationService;
 import com.database.atypon.Node.utils.response.Response;
-import com.database.atypon.Node.model.User;
 import com.database.atypon.Node.utils.response.ResponseType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.WebUtils;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
@@ -32,21 +29,5 @@ public class AuthenticationController {
         String token = authenticationService.generateToken(user);
         res.setContent(token);
         return res;
-    }
-    @PostMapping("/logout")
-    public Response logout(HttpServletRequest request, HttpServletResponse response) {
-
-        String token;
-        try{
-            token = String.valueOf(WebUtils.getCookie(request, "token"));
-        }catch (Exception e){
-            return new Response(ResponseType.ERROR, "You are not logged in");
-        }
-        if (token == "null")
-            return new Response(ResponseType.ERROR, "You are not logged in");
-        Cookie cookie = authenticationService.buildTokenCookie(token);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-        return new Response(ResponseType.SUCCESS, "Logout successfully");
     }
 }
